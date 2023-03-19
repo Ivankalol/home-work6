24b2c8977a9af260da9888a18f2d00ced97fce44
ivan <korivan0900@mail.ru>
2023-03-20 00:19:18 +0300

    home work6

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// int[] InputArray(int size)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.WriteLine($"input number {i+1} :");
+//         string num = Console.ReadLine();
+//         array[i] = Convert.ToInt32(num);
+//     }
+//     return array;
+// }
+
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write (array[i] + " ");
+//     }
+//     Console.WriteLine ();
+// }
+
+// int CountPositiveNumbers(int[] array)
+// {
+//     int count = 0; 
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] > 0)
+//         {
+//             count++; 
+//         }
+//     }
+//     return count;
+// }
+
+// Console.WriteLine ("Input array size:");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] myArray = InputArray(size);
+// Console.WriteLine($"Array:");
+// ShowArray(myArray);
+// Console.WriteLine($"positive number ->{CountPositiveNumbers(myArray)}");
+
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+
